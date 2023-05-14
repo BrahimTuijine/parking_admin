aaeb93d6edd56c5050b1e7cf84c24226e739b16a
brahim tuijine <brahim.tuijne@slnee.com>
2023-05-14 23:53:57 +0100

    check login done

--- lib/core/router/route.dart
+++ lib/core/router/route.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pfe_parking_admin/screens/dashboard/app_scaffold.dart';
 import 'package:pfe_parking_admin/screens/login/normal_login.dart';
+import 'package:pfe_parking_admin/services/firebase_auth.dart';
 
 final rootNavigatorKey = GlobalKey<NavigatorState>();
 final shellNavigatorKey = GlobalKey<NavigatorState>();
@@ -9,7 +11,9 @@ final shellNavigatorKey = GlobalKey<NavigatorState>();
 class AppRouter {
   static final GoRouter router = GoRouter(
     navigatorKey: rootNavigatorKey,
-    initialLocation: '/normalLogin',
+    initialLocation: FirebaseAuth.instance.currentUser?.uid == null
+        ? '/normalLogin'
+        : '/parkings',
     routes: [
       GoRoute(
         path: '/normalLogin',
